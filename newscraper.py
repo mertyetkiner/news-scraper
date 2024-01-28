1db91cda5ca23bd04df20470349ad11f0ac11c59
Niyazi Mert Yetkiner <49085939+mertyetkiner@users.noreply.github.com>
2024-01-28 22:41:58 +0300

    Update newscraper.py

--- newscraper.py
+++ newscraper.py
@@ -1,33 +1,56 @@
-from requests_html import HTMLSession # import HTMLSession class from requests_html module
+# import HTMLSession class from requests_html module
+from requests_html import HTMLSession
 
-def clean_text(text): # Function for text clearing
+# Function for text clearing
+def clean_text(text):
     return text.replace('\n', ' ').replace('\xa0', ' ')
 
-session = HTMLSession() # Create an object of class HTMLSession
-url = 'https://www.ntv.com.tr/' # URL of the website to be scraped
+# Create an object of class HTMLSession
+session = HTMLSession()
 
+# URL of the website to be scraped
+url = 'https://www.ntv.com.tr/' 
+
+# Request web page
 try:
-    r = session.get(url) # Request web page
-    r.html.render(sleep=1,) # Pull rendered content with JavaScript by spooling
-    articles = r.html.find('div.ntv-main-slider-item') # Find all elements with a given class on a web page
+    r = session.get(url)
 
-    newslist = [] # Create a list to hold the information of the news
+    # Pull rendered content with JavaScript by spooling
+    r.html.render(sleep=1,)
 
-    for item in articles:  # Loop for each element
+    # Find all elements with a given class on a web page    
+    articles = r.html.find('div.ntv-main-slider-item')
+
+    # Create a list to hold the information of the news
+    newslist = []
+    
+    # Loop for each element
+    for item in articles:
         try:
-            newsitem = item.find("a", first=True) # Pull news title and link from each element
-            title = clean_text(newsitem.attrs['title'])  # Clear the news headline using the clear function
-            newsarticle = {  # Collect news title and link in a dictionary
+            # Pull news title and link from each element
+            newsitem = item.find("a", first=True)
+
+            # Clear the news headline using the clear function
+            title = clean_text(newsitem.attrs['title'])
+
+            # Collect news title and link in a dictionary
+            newsarticle = {
                 'title': title,
                 'link' : newsitem.absolute_links
             }
-            newslist.append(newsarticle) # Add news to the list
-        except KeyError as e: # Print to screen if KeyError error is received
-            print(f"KeyError: {e}")
 
-    for news in newslist: # Print the information of each news item on the screen
+            # Add news to the list
+            newslist.append(newsarticle)
+        
+        except KeyError as e:
+            # Print to screen if KeyError error is received
+            print(f"KeyError: {e}")
+            
+    # Print the information of each news item on the screen
+    for news in newslist:
         print(news)
         print()
 
-except Exception as e: # In case of any error, print the error to the screen
+# In case of any error, print the error to the screen
+except Exception as e:
     print(f"An error occurred: {e}")
