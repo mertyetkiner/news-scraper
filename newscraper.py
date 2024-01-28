6e7ab3dc1e613f0f47710e28513e0a5a40e0e8fb
Niyazi Mert Yetkiner <49085939+mertyetkiner@users.noreply.github.com>
2024-01-28 22:20:09 +0300

    Update newscraper.py

--- newscraper.py
+++ newscraper.py
@@ -1,33 +1,33 @@
-from requests_html import HTMLSession # requests_html modülünden HTMLSession sınıfını içe aktar
+from requests_html import HTMLSession # import HTMLSession class from requests_html module
 
-def clean_text(text): # Text temizleme işlemi için fonksiyon
+def clean_text(text): # Function for text clearing
     return text.replace('\n', ' ').replace('\xa0', ' ')
 
-session = HTMLSession() # HTMLSession sınıfından bir nesne oluştur
-url = 'https://www.ntv.com.tr/' # Scraping işlemi yapılacak web sitesinin URL'si
+session = HTMLSession() # Create an object of class HTMLSession
+url = 'https://www.ntv.com.tr/' # URL of the website to be scraped
 
 try:
-    r = session.get(url) # Web sayfasını talep et
-    r.html.render(sleep=1,) # JavaScript ile render edilen içeriği bekleterek çek
-    articles = r.html.find('div.ntv-main-slider-item')  # Web sayfasındaki belirli bir sınıfa sahip tüm elemanları bul
+    r = session.get(url) # Request web page
+    r.html.render(sleep=1,) # Pull rendered content with JavaScript by spooling
+    articles = r.html.find('div.ntv-main-slider-item') # Find all elements with a given class on a web page
 
-    newslist = [] # Haberlerin bilgilerini tutacak bir liste oluştur
+    newslist = [] # Create a list to hold the information of the news
 
-    for item in articles:  # Her bir eleman için döngü oluştur
+    for item in articles:  # Loop for each element
         try:
-            newsitem = item.find("a", first=True) # Her bir elemandan haber başlığını ve linki çek
-            title = clean_text(newsitem.attrs['title'])  # Temizleme fonksiyonunu kullanarak haber başlığını temizle
-            newsarticle = {  # Haber başlığı ve bağlantısını bir sözlükte topla
+            newsitem = item.find("a", first=True) # Pull news title and link from each element
+            title = clean_text(newsitem.attrs['title'])  # Clear the news headline using the clear function
+            newsarticle = {  # Collect news title and link in a dictionary
                 'title': title,
                 'link' : newsitem.absolute_links
             }
-            newslist.append(newsarticle) # Haberleri listeye ekle
-        except KeyError as e: # KeyError hatası alınırsa ekrana yazdır
+            newslist.append(newsarticle) # Add news to the list
+        except KeyError as e: # Print to screen if KeyError error is received
             print(f"KeyError: {e}")
 
-    for news in newslist: # Her bir haberin bilgilerini ekrana yazdır
+    for news in newslist: # Print the information of each news item on the screen
         print(news)
         print()
 
-except Exception as e: # Herhangi bir hata durumunda ekrana hatayı yazdır
+except Exception as e: # In case of any error, print the error to the screen
     print(f"An error occurred: {e}")
